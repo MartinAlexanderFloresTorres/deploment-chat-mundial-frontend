73dfee93e1769fd93be0ed949e2352bba7b42243
Martin Alexander Flores Torres <martinflorestorres21@gmail.com>
2022-08-04 00:32:10 -0500

    version 1

--- src/context/MensajesProvider.js
+++ src/context/MensajesProvider.js
@@ -1,8 +1,8 @@
 import { createContext, useState, useEffect } from "react";
 import io from "socket.io-client";
-/* const socket = io("http://localhost:4000"); */
-const socket = io("https://chat-mundial.herokuapp.com");
 
+//=============== conectar con el backend ===============//
+const socket = io("http://localhost:4000");
 const MensajesContext = createContext();
 
 const MensajesProvider = ({ children }) => {
